889fc3f30dd70ea5eb8eb0e1d7056c89b7bb96d6
dany61955 <98032858+dany61955@users.noreply.github.com>
2023-10-20 14:50:22 +0530

    Update juniper_srx_set_to_json.py

--- juniper_srx_set_to_json.py
+++ juniper_srx_set_to_json.py
@@ -2,46 +2,38 @@ import re
 import json
 
 def set_to_json(input_file, output_file):
-    # Regular expressions to match set, delete, and hierarchical set lines
-    set_pattern = r'^set (\S+ (.+))$'
-    delete_pattern = r'^delete (\S+ (.+))$'
-    hierarchy_pattern = r'^set (\S+)$'
+    def add_to_dict(d, path, value):
+        if len(path) == 1:
+            d[path[0]] = value
+        else:
+            key = path[0]
+            if key not in d:
+                d[key] = {}
+            add_to_dict(d[key], path[1:], value)
 
-    # Initialize an empty configuration dictionary
     config = {}
+    current_path = []
 
     with open(input_file, 'r') as file:
-        current_node = config
         for line in file:
             line = line.strip()
-            
-            # Match a set statement
-            set_match = re.match(set_pattern, line)
-            if set_match:
-                path, value = set_match.groups()
-                parts = path.split()
-                for part in parts:
-                    current_node = current_node.setdefault(part, {})
-                current_node = value
-
-            # Match a delete statement
-            delete_match = re.match(delete_pattern, line)
-            if delete_match:
-                path, value = delete_match.groups()
-                parts = path.split()
-                current_node = config
-                for part in parts:
-                    current_node = current_node.setdefault(part, {})
-                if value in current_node:
-                    del current_node[value]
-
-            # Match a hierarchical set statement
-            hierarchy_match = re.match(hierarchy_pattern, line)
-            if hierarchy_match:
-                path = hierarchy_match.group(1)
-                parts = path.split()
-                for part in parts:
-                    current_node = current_node.setdefault(part, {})
+            if line.startswith("set "):
+                set_match = re.match(r'set (.+)', line)
+                if set_match:
+                    path = set_match.group(1).split()
+                    current_path = path
+                else:
+                    add_to_dict(config, current_path, line)
+            elif line.startswith("delete "):
+                delete_match = re.match(r'delete (.+)', line)
+                if delete_match:
+                    path = delete_match.group(1).split()
+                    if path:
+                        key = path[-1]
+                        if key in config:
+                            del config[path[0]]
+            else:
+                current_path = []
 
     # Write the JSON to the output file
     with open(output_file, 'w') as outfile:
